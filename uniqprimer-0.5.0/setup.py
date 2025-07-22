f2ee6ef3210559c77e82a13c6f298d2c0915820c
RamilBenitez <ramsbenitez21@gmail.com>
2025-07-22 09:19:08 +0800

    refactor: changes to python 3 compatibility

--- uniqprimer-0.5.0/setup.py
+++ uniqprimer-0.5.0/setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup # rams added setuptools is the current standard
 import distutils.command.install
 import os
 
-class create_link(distutils.core.Command):
+#rams deleted the entire class, it is empty and non-functional
+# class create_link(distutils.core.Command):
     
-    def run( self ):
-        #create a sym link to uniqprimer.py inside of /usr/local/bin
-       os.symlink(  ) 
+#     def run( self ):
+#         #create a sym link to uniqprimer.py inside of /usr/local/bin
+#        os.symlink(  ) 
         
 
 setup( name='uniqprimer',
@@ -16,7 +18,7 @@ setup( name='uniqprimer',
        version='0.5.0',
        packages=[ 'primertools' ],
        scripts=[ 'uniqprimer.py' ],
+       python_requires = '>=3.6', #rams added for installation
        data_files=[ ( '/usr/local/bin', [ 'uniqprimer.py' ] ) ] 
-       
        )
 
